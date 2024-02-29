01a0b26644619705f8367a48ce500c04c4a3565b
Ammar Ahmed <dirleader123@gmail.com>
2024-02-29 15:09:11 +0500

    Airthmathic Operators

---
+++ Airmathic-Operators/app.js
@@ -0,0 +1,158 @@
+// Airmathic Operators:  
+// Q NO : 1
+// ANS NO: 1
+
+var x = 25
+var y = 7
+
+console.log(x % y)
+// Q NO : 2
+// ANS NO: 2
+
+var a = 15
+var b = 4
+var a = 15 + 10 
+var b = 4 - 2
+
+console.log(a * b)
+
+// Q NO : 3
+// ANS NO: 3
+
+var c = 12 
+var d = 3
+
+console.log(c ** d)
+
+
+// Q NO : 4
+// ANS NO: 4
+
+
+var e = 10
+var f = 3
+var c = f * 2
+console.log(c + e)
+
+
+// Q NO : 5
+// ANS NO: 5
+
+var g = 20
+var h = 4
+var m = g + 5
+
+console.log(m / h)
+
+
+// Q NO : 6
+// ANS NO: 6
+
+var i = 8
+var j = 3
+var x = i - 2
+
+console.log(x * j)
+
+
+// Q NO : 7
+// ANS NO: 7
+
+var k = 18
+var l = 5
+var v =  k - l 
+ 
+console.log(v + 2)
+
+
+
+// Q NO : 8
+// ANS NO: 8
+
+var m = 7
+var n = 2
+
+
+console.log(m ** n)
+
+
+// Q NO : 9
+// ANS NO: 9
+
+var o = 13
+var p = 4
+var c = p + 2
+
+console.log(o % c)
+
+
+
+// Q NO : 10
+// ANS NO: 10
+
+var q = 16     
+var r = 3
+var s = q / 2
+
+console.log(s * r)
+
+
+
+// Q NO : 11
+// ANS NO: 11
+
+var s = 25
+var t = 3
+var n = s - 1
+
+console.log(n / t)
+
+
+
+
+
+// Q NO : 12
+// ANS NO: 12
+
+var u = 11
+var v = 3
+var x = v + 2
+
+console.log(u - x)
+
+
+
+// Q NO : 13
+// ANS NO: 13
+
+var w = 22
+var x = 5
+var n = x ** 2
+
+console.log(w + n)
+
+
+
+// Q NO : 14
+// ANS NO: 14
+
+var y = 19
+var z = 4
+var m = z + 1
+
+console.log(y % m)
+
+
+
+// Q NO : 15
+// ANS NO: 15
+
+var a = 10
+var b = 3
+var c = b - 2
+
+console.log(a * c)
+
+// Airmathic Operators:  THE END
+
+
