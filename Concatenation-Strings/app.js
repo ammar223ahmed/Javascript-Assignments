1e09306c4bad02a789c63b91c37a82b791645751
Ammar Ahmed <dirleader123@gmail.com>
2024-03-02 15:27:28 +0500

    UnFamilier Operators and Concatenation Strings

---
+++ Concatenation-Strings/app.js
@@ -0,0 +1,88 @@
+// Concatenation Strings Excersixe:  
+// ANS NO: 1
+
+
+var firstName = 'Ammar'
+var lastName = 'Ahmed'
+var fulName = firstName + " " + lastName
+
+console.log(fulName)
+
+
+// ANS NO: 2
+
+var world = 'Pakistan'
+var city = 'karachi'
+var worldCity = world + " " + city
+
+console.log(worldCity)
+
+// ANS NO: 3
+
+var fruit = 'Apple'
+var shakes = 'Juice'
+var fruitShakes = fruit + " " + shakes
+
+console.log(fruitShakes)
+
+// ANS NO: 4
+
+var zamin = 'Allah ne'
+var ashman = 'banaye'
+var zaminAshman = zamin + " " + ashman
+
+console.log(zaminAshman)
+
+// ANS NO: 5
+
+var sen = 'Javascript is the programing language'
+var tense = 'of the web.'
+var sentence = sen + " " + tense
+
+console.log(sentence)
+
+// ANS NO: 6
+
+var  technology = 'Open'
+var company =  'Source'
+var names  = 'Intelligence'
+var technologyCompany = technology + " " + company + " " + names
+
+console.log(technologyCompany)
+
+// ANS NO: 7
+
+var wishMe = 'Happy'
+var birth = 'Birthday'
+var day = 'to you'
+var myBirthday = wishMe + " " + birth + " " + day 
+console.log(myBirthday)
+
+// ANS NO: 8
+
+var goo = "Good luck"
+var  luc = "have fun"
+var goodLuck = goo + " " + luc
+console.log(goodLuck)
+
+// ANS NO: 9
+
+var mySk = "Web Design"
+var myskil = "and Developnment"
+var mySkils = mySk + " " + myskil
+console.log(mySkils)
+
+// ANS NO: 10
+
+var comp = 'Artificial'
+var plex = 'Neural'
+var terms = 'Networks'
+var complexTechnology = comp + " " + plex + " " + terms
+
+console.log(complexTechnology)
+
+
+
+// Concatenation Strings:  THE END
+
+
---
+++ UnFamilier-Operators/app.js
@@ -0,0 +1,79 @@
+// UnFamilier Operators:  
+// ANS NO: 1
+
+var poNum = 8
+var posNum = poNum++
+
+console.log(poNum)
+
+
+// ANS NO: 2
+
+var x = 15 
+var y = --x
+
+console.log(y)
+
+
+// ANS NO: 3
+
+var Num = 25
+var pNum = Num--
+
+
+console.log(Num)
+
+
+// ANS NO: 4
+
+var start = 10 
+var jj = ++start
+
+console.log(start)
+
+
+// ANS NO: 5
+var index = 5
+var aIndex = ++index
+
+console.log(index)
+
+// ANS NO: 6
+
+var quantity = 12
+var assign = --quantity
+
+console.log(assign)
+
+// ANS NO: 7
+
+var counter = 18
+var aCounter = counter++
+
+console.log(counter)
+
+// ANS NO: 8
+
+var score = 30
+var aStore = score--
+
+console.log(score)
+
+// ANS NO: 9
+
+var value = 20
+++value
+console.log(value)
+
+// ANS NO: 10
+
+var counters = 8
+counters--
+
+console.log(counters)
+
+
+
+// UnFamilier Operators:  THE END
+
+
